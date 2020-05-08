6c5174202388e8d33988a06e885ae0875cae7269
Octavio Laraia <o.laraia@web.de>
2020-05-08 20:56:32 +0200

    TabuTimer added

--- src/main/java/hdhotdog/advi/plugins/TabuTimer.java
+++ src/main/java/hdhotdog/advi/plugins/TabuTimer.java
@@ -1,12 +1,33 @@
 package hdhotdog.advi.plugins;
 
 public class TabuTimer implements Runnable {
-    public Tabu tabu;
-    public TabuTimer(Tabu tabu) {
-        this.tabu = tabu;
+    public TabuGame tabuGame;
+    private boolean running;
+    private Thread thread;
+
+    public TabuTimer(TabuGame tabuGame) {
+        this.tabuGame = tabuGame;
+        this.thread = new Thread(this);
     }
 
+    public void start() {
+        this.running = true;
+        if(running) {
+            this.thread.start();
+        }
+    }
     public void run() {
-
+        try {
+            thread.wait(30000);
+            thread.wait(30000);
+            thread.wait(30000);
+            thread.wait(30000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    public void stop() {
+        this.running = false;
+        this.thread.stop();
     }
 }
