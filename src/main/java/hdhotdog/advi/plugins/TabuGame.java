d660131a7c8d5f420d9574b6a839dc50d5c84806
THE-ORONCO <theo.roncoletta@poste.net>
2020-05-08 21:30:40 +0200

    small shit

--- src/main/java/hdhotdog/advi/plugins/TabuGame.java
+++ src/main/java/hdhotdog/advi/plugins/TabuGame.java
@@ -1,17 +1,19 @@
 package hdhotdog.advi.plugins;
 
 import org.bukkit.entity.Player;
-import sun.tools.jconsole.Tab;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.Map;
 
 public class TabuGame {
-    public HashSet<TabuPlayer> players = new HashSet<TabuPlayer>();
+    private Hashtable<String, TabuPlayer> players;
     private String name;
     private int rounds;
     private int gameID;
     private static int games = 0;
+    private TabuPlayer creator;
 
     public TabuGame(String name, int rounds) {
         this.name = name;
@@ -19,17 +21,29 @@ public class TabuGame {
         this.gameID = games;
         games++;
 
+        this.players = new Hashtable<>();
     }
 
     public TabuGame(String name) {
         this(name, 3);
     }
 
-    public TabuGame(){
+    public TabuGame() {
         this("Tabu-Game" + games, 3);
     }
 
+    public boolean addPlayer(Player player) {
+        boolean playerAlreadyAdded = this.players.containsKey(player.getName());
+        if (!playerAlreadyAdded) {
+            players.put(player.getName(), new TabuPlayer(player));
+            return true;
+        }
+        return false;
+    }
 
+    public boolean removePlayer() {
+
+    }
 
 
 }
